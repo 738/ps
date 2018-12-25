b63862fd3891c16d49646c0eb25284812ac9a63f
Jon Jee <jydrogen@gmail.com>
2018-12-25 23:36:55 +0900

    add 2839 설탕배달

--- ps/2747.py
+++ ps/2747.py
@@ -1,14 +1,26 @@
 # 2747 피보나치 수
 # 20181225
 
-from sys import stdin, stdout
-input = stdin.readline
-print = stdout.write
+# from sys import stdin, stdout
+# input = stdin.readline
+# print = stdout.write
+#
+# arr = [0, 1]
+#
+# for i in range(2, 1000000000):
+#     arr.append(arr[i - 1] + arr[i - 2])
+#
+# n = int(input())
+# print(str(arr[n])+"\n")
 
-arr = [0, 1]
+# print('|\_/|')
+# print('|q p|   /}')
+# print('( 0 )"""\\')
+# print('|"^"`    |')
+# print('||_/=\\\\__|')
 
-for i in range(2, 46):
-    arr.append(arr[i - 1] + arr[i - 2])
-
-n = int(input())
-print(str(arr[n])+"\n")
+while True:
+    try:
+        print(input())
+    except EOFError:
+        break
---
+++ ps/2839.py
@@ -0,0 +1,30 @@
+# 2839. 설탕 배달
+# 181225
+
+import sys
+sys.setrecursionlimit(3000)
+
+arr = [0] * 5001
+
+# 점화식: D(n) = min(D(n-3), D(n-5)) + 1
+def dp(num):
+    if num in [3, 5]:
+        return 1
+    elif num in [1, 2, 4]:
+        return -1
+    elif arr[num] != 0:
+        return arr[num]
+    a = dp(num-3)
+    b = dp(num-5)
+    if a == -1 and b == -1:
+        arr[num] = -1
+    elif a == -1:
+        arr[num] = b + 1
+    elif b == -1:
+        arr[num] = a + 1
+    else:
+        arr[num] = min(a, b) + 1
+    return arr[num]
+
+n = int(input())
+print(dp(n))
