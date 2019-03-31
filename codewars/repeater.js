30f49ef59feeb370a5716cace3a0a70b0fb8e3bd
Jon Jee <jydrogen@gmail.com>
2019-03-31 22:28:04 +0900

    Create repeater.js

---
+++ codewars/repeater.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/thinkful-string-drills-repeater/train/javascript
+repeater=(s,n)=>Array(n).fill(s).join``
+// String.prototype.repeat이라는 함수가 있었음
+// const repeater = Function.call.bind("".repeat); 생각해볼 코드
