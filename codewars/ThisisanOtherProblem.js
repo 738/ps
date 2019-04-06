f6ed73cd5462de5f8717e4e9a5c04b68e8e3f91c
Jon Jee <jydrogen@gmail.com>
2019-04-06 21:54:19 +0900

    add best practice

--- codewars/ThisisanOtherProblem.js
+++ codewars/ThisisanOtherProblem.js
@@ -29,3 +29,24 @@ Object.defineProperty(d, 'fullName', {
     this.lastName = full.split` `[1];
   }
 });
+
+// best practice (other person)
+
+// function NamedOne(first, last) {
+//   this.firstName = first;
+//   this.lastName = last;
+
+//   Object.defineProperty(this, "fullName", {
+//     set: function(value) {
+//       var parts = value.split(" ");
+//       if (parts.length === 2) {
+//         this.firstName = parts[0];
+//         this.lastName = parts[1];
+//       }
+//     },
+//     get: function() {
+//       return this.firstName + ' ' + this.lastName;
+//     }
+//   });
+// }
+
