b8ad806ac20006c15e9e43ba19ce1bcdfd8335f1
Jon Jee <jydrogen@gmail.com>
2019-05-25 22:48:15 +0900

    add 9461.py

--- src/9461.py
+++ src/9461.py
@@ -1,6 +1,2 @@
-n=int(input())
-d=[0,1,1,1,2,2]
-for i in range(6,101):
-    d.append(d[i-1]+d[i-5])
-for i in range(n):
-    print(d[int(input())])
+d=[*[1]*4,2,2]
+exec("d+=[d[-1]+d[-5]];"*95+"print(d[int(input())]);"*int(input()))
