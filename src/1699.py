d00115e8d970e436942e61f4d99e6c0ab6ae8110
Jon Jee <jydrogen@gmail.com>
2019-05-25 21:48:18 +0900

    update 1699.py

--- src/1699.py
+++ src/1699.py
@@ -1,15 +1,7 @@
-# d=[i**2 for i in range(317)]
-# def get_square_number(x):
-#     for i in range(316, 0, -1):
-#         if x >= d[i]:
-#             return i**2
-
 n=int(input())
-dp = [0,1];
-for i in range(2, n+1):
-    p=0
+dp=[0]*(n+1)
+for i in range(1,n+1):
     for j in range(1,int(i**(1/2))+1):
-        if p > dp[i - j*j] + 1 or p == 0:
-            p=dp[i - j*j] + 1
-    dp.append(p)
+        if dp[i] > dp[i-j*j]+1 or dp[i] == 0:
+            dp[i]=dp[i-j*j]+1
 print(dp[n])
