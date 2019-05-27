59acc6ad7ab8232932ec38b3f83f2f42882c1936
Jon Jee <jydrogen@gmail.com>
2019-05-27 23:29:31 +0900

    update 11650 code golf

--- src/11650.py
+++ src/11650.py
@@ -1,4 +1,1 @@
-n=int(input())
-m=[tuple(map(int,input().split()))for i in range(n)]
-m.sort(key=lambda x:(x[0],x[1]))
-[print(*m[i]) for i in range(n)]
+[print(*v)for v in sorted([[*map(int,input().split())]for i in range(int(input()))])]
