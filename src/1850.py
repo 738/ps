7b6e177eba88c43c6490cc62eeea4337f76d878b
Jon Jee <jydrogen@gmail.com>
2019-05-28 22:44:06 +0900

    update 1850.py

--- src/1850.py
+++ src/1850.py
@@ -1,11 +1,3 @@
-def gcd(a, b):
-    if a < b: a, b = b, a
-    while a % b != 0:
-        a, b = b, a%b
-    return b
-
-import sys
-input = sys.stdin.readline
-a, b = map(int, input().split())
-
-print("1"*gcd(a,b))
+a,b=map(int,input().split())
+while b:a,b=b,a%b
+print("1"*a)
