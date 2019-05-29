cef31eaf1c9833844a6e303d391641a3ef7824f2
Jon Jee <jydrogen@gmail.com>
2019-05-29 23:48:53 +0900

    add 2004.py

--- src/2004.py
+++ src/2004.py
@@ -1,14 +1,31 @@
 import sys
-sys.setrecursionlimit(2000)
+input=sys.stdin.readline
 
-def fact(x): return x*fact(x-1) if x > 1 else 1
-def C(n,r): return fact(n)//(fact(r)*fact(n-r))
-n,r=map(int,input().split())
-a=str(C(n,r))
-s=0
-for v in a[::-1]:
-    if v == "0":
-        s+=1
-    else:
-        break
-print(s)
+def countTwo(n):
+    two=0
+    for i in range(2,n+1,2):
+        two += n//i
+    return two
+    
+def countFive(n):
+    five=0
+    i=5
+    while i <= n:
+        five += n//i
+        i *= 5
+    return five
+
+n,m=map(int, input().split())
+print(min(countTwo(n)-countTwo(m)-countTwo(n-m), countFive(n)-countFive(m)-countFive(n-m)))
+
+# def f(n):
+#     two,five=0,0
+#     for i in range(2,n+1,2):
+#         two += n // i
+#     for i in range(5,n+1,5):
+#         five += n // i
+#     return two,five
+# import sys
+# input=sys.stdin.readline
+# n,m=map(int,input().split())
+# print(min(f(n)[0] - f(m)[0] - f(n-m)[0], f(n)[1] - f(m)[1] - f(n-m)[1]))
