3512407d7ca5deccdfc01e45190490fe456d4c27
Junwoo Ji <jydrogen@gmail.com>
2019-10-19 00:53:55 +0900

    Update 1181.js

--- src/1181.js
+++ src/1181.js
@@ -1,7 +1,4 @@
 let n=require('fs').readFileSync('dev/stdin').toString().trim().split('\n')
 n.shift()
-n.sort((a,b) => {
-  if (a.length !== b.length) return a.length - b.length
-  return a.localeCompare(b)
-})
+n.sort((a,b)=>a.length-b.length||a.localeCompare(b))
 Array.from(new Set(n)).forEach(v=>console.log(v))
