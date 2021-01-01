42b52152360550e2b0ff1d2e543d73240d8b2b4c
Junwoo Ji <jydrogen@gmail.com>
2021-01-01 14:15:23 +0900

    Update 344.js

--- leetcode/344.js
+++ leetcode/344.js
@@ -3,5 +3,7 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    return s.reverse();
+    for (let i=0; i<s.length/2; i++) {
+        [s[i], s[s.length - i - 1]] = [s[s.length - i - 1], s[i]];
+    }
 };
